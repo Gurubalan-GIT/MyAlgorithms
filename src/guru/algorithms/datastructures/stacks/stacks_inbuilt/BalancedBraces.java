d357681fd614887d0b949d7f35433991818e4c93
Gurubalan-GIT <gurubalan.work@gmail.com>
2019-01-24 02:07:53 +0530

    Updated Balancing Parentheses.

--- src/guru/algorithms/datastructures/stacks/stacks_inbuilt/BalancedBraces.java
+++ src/guru/algorithms/datastructures/stacks/stacks_inbuilt/BalancedBraces.java
@@ -5,20 +5,33 @@ import java.util.Stack;
 
 public class BalancedBraces {
     public static final Scanner sc=new Scanner(System.in);
-    public  static void main(String[] args){
-        Stack<Character> braceStack = new Stack<>();
-        String braceString=sc.nextLine();
-        int closeFound=0;
-        for(int i=0;i<braceString.length();i++){
-            if(braceString.charAt(i)=='{') {
-                braceStack.push(braceString.charAt(i));
+    public static void main(String[] args) {
+        String parenthesesString = sc.nextLine();
+        System.out.println(checkBalancedParentheses(parenthesesString));
+    }
+    private static String checkBalancedParentheses(String expr)
+    {
+        if (expr.isEmpty())
+            return "Balanced";
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < expr.length(); i++)
+        {
+            char current = expr.charAt(i);
+            if (current == '{' || current == '(' || current == '[')
+            {
+                stack.push(current);
+            }
+            if (current == '}' || current == ')' || current == ']')
+            {
+                if (stack.isEmpty())
+                    return "Not Balanced";
+                char last = stack.peek();
+                if (current == '}' && last == '{' || current == ')' && last == '(' || current == ']' && last == '[')
+                    stack.pop();
+                else
+                    return "Not Balanced";
             }
         }
-        for(int i=0;i<braceString.length()-1;i++){
-           closeFound = braceStack.search('}');
-        }
-        System.out.println(braceStack);
-        System.out.println(closeFound);
+        return stack.isEmpty()?"Balanced":"Not Balanced";
     }
-
 }
