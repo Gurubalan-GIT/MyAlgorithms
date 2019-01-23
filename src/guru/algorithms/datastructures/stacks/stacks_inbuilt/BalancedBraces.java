43cc521d87985ce01924577ab97ea3ef78d4fb00
Gurubalan-GIT <gurubalan.work@gmail.com>
2019-01-24 02:00:46 +0530

    Pushed changes.

--- src/guru/algorithms/datastructures/stacks/stacks_inbuilt/BalancedBraces.java
+++ src/guru/algorithms/datastructures/stacks/stacks_inbuilt/BalancedBraces.java
@@ -1,5 +1,24 @@
 package guru.algorithms.datastructures.stacks.stacks_inbuilt;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class BalancedBraces {
+    public static final Scanner sc=new Scanner(System.in);
+    public  static void main(String[] args){
+        Stack<Character> braceStack = new Stack<>();
+        String braceString=sc.nextLine();
+        int closeFound=0;
+        for(int i=0;i<braceString.length();i++){
+            if(braceString.charAt(i)=='{') {
+                braceStack.push(braceString.charAt(i));
+            }
+        }
+        for(int i=0;i<braceString.length()-1;i++){
+           closeFound = braceStack.search('}');
+        }
+        System.out.println(braceStack);
+        System.out.println(closeFound);
+    }
 
 }
