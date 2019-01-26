fc3d58529c92233cd3102d828831bc4f1743bf66
Gurubalan-GIT <gurubalan.work@gmail.com>
2019-01-26 18:12:36 +0530

    Updated Balancing Parentheses.

--- src/guru/algorithms/datastructures/stacks/stacks_inbuilt/BalancedBraces.java
+++ src/guru/algorithms/datastructures/stacks/stacks_inbuilt/BalancedBraces.java
@@ -11,21 +11,25 @@ public class BalancedBraces {
     }
     private static String checkBalancedParentheses(String expr)
     {
+        //Check if stack is already empty
         if (expr.isEmpty())
             return "Balanced";
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < expr.length(); i++)
         {
             char current = expr.charAt(i);
+            //Checking if any open parentheses have occurred, if so we are pushing into the stack
             if (current == '{' || current == '(' || current == '[')
             {
                 stack.push(current);
             }
             if (current == '}' || current == ')' || current == ']')
             {
+                //Now for closing parentheses, there should be a set of opening parentheses, hence if stack is empty it is imbalanced
                 if (stack.isEmpty())
                     return "Not Balanced";
                 char last = stack.peek();
+                //We are using peek to see the top element of the stack and compare it with the opening parentheses, if it matches, then we are popping
                 if (current == '}' && last == '{' || current == ')' && last == '(' || current == ']' && last == '[')
                     stack.pop();
                 else
