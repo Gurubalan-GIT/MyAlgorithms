d871b2e5ca943f4d398c3a7c44b9d8577e33d6bd
Gurubalan-GIT <gurubalan.work@gmail.com>
2019-01-30 21:32:56 +0530

    Bubble sort SLL fix.

--- src/Java/datastructures/linkedlist/linkedlist_trivial/SLLSort.java
+++ src/Java/datastructures/linkedlist/linkedlist_trivial/SLLSort.java
@@ -3,8 +3,13 @@ package Java.datastructures.linkedlist.linkedlist_trivial;
 public class SLLSort {
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
+        System.out.println("Enter number of nodes-");
         int nodes = LinkedList.scanner.nextInt();
+        System.out.println("Enter the values:");
         linkedList.addMultipleLast(nodes);
+        System.out.println("Before Sorting- ");
+        linkedList.show();
+        System.out.println("After Sorting- ");
         linkedList.sort();
         linkedList.show();
     }
